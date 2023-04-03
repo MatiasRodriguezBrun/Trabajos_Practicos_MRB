970e7e3605d66ec507dc8fcc4d3b54f2678d1262
MatiasRodriguezBrun <matias.rbrun@gmail.com>
2023-04-03 09:09:34 -0300

    cambio de nombre de prueba a bio

--- Clases2año/Tareas/swap.py
+++ Clases2año/Tareas/swap.py
@@ -12,12 +12,17 @@ os.rename(file1, "temporal")
 os.rename(file2, file1)
 os.rename("temporal", file2)
 
+# tambien se puede hacer desde la terminal de la siguinte manera
+# problema: renombra un archivo existente 
+# mv hola.txt temporal.txt 
+# mv chau.txt hola.txt
+# mv temporal.txt chau.txt
+
 """
 tareas : modificar este scrip para que pueda incorporar los nombres de los
 archivos desde la terminal
 usar (os.path.exists)--> path 
 ver como se usa para mejorar el scrip (basicamente corrobora que el archivo exista)
-
 """
 
 
