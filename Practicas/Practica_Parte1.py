377e3dd2dc78dbbe3d61da1bb43f14447a68ddd2
MatiasRodriguezBrun <matias.rbrun@gmail.com>
2023-04-08 16:36:24 -0300

    Agregue unos print() para que las respuestas queden mas prolijas y cambie algunos print por return

--- Practicas/Practica_Parte1.py
+++ Practicas/Practica_Parte1.py
@@ -9,12 +9,14 @@ print(siguiente(10))
 def anterior(numero):
     return numero - 1
 print(anterior(10))
+print()
 
 #Ejercicio 2
 # Escribir una función que obtenga el doble de un número.
 def doble(numero):
     return numero*2
 print (doble(10))
+print()
 
 #Ejercicio 3
 # Escribir funciones que obtengan el doble del anterior y el doble del siguiente de un número.
@@ -25,6 +27,7 @@ print (doble_del_anterior(3))
 def doble_del_siguiente(numero):
     return (numero +1)*2
 print (doble_del_siguiente(3))
+print()
 
 #Ejercicio 4
 # Escribir una función llamada retirar_dinero, que tenga como parámetros el saldo y el monto a retirar y que 
@@ -33,6 +36,7 @@ print (doble_del_siguiente(3))
 def retirar_dinero(saldo,monto):
     return max (saldo-monto,0)
 print (retirar_dinero(30,50))
+print()
 
 #Ejercicio 5
 # Escribir una función llamada dia_de_la_semana_favorito que tome como parámetro un día de la semana y 
@@ -42,6 +46,7 @@ def dia_de_la_semana_favorito(dia):
     False, "Sábado": True, "Domingo": False}.get(dia, False)
 print(dia_de_la_semana_favorito("Lunes"))
 print(dia_de_la_semana_favorito("Sábado"))
+print()
 
 #Ejercicio 6
 # Escribir una función que determine si una longitud de onda de una radio está dentro o fuera del rango de 
@@ -50,6 +55,7 @@ def rango_de_recepcion(longitud_de_onda):
     return 223.0 <= longitud_de_onda <= 586.8
 print(rango_de_recepcion(300))
 print(rango_de_recepcion(40))
+print()
 
 #Ejercicio 7
 # Reescribir la función del punto anterior considerando, además, que la longitud de onda no puede ser 314.5
@@ -62,6 +68,7 @@ print(rango_de_recepcion(314.5))
 longitud = 10
 mensaje = "La longitud de onda está dentro del rango de recepción" * rango_de_recepcion(longitud) or "La longitud de onda está fuera del rango de recepción"
 print(mensaje)
+print()
 
 #Ejercicio 8
 # Escribir una función llamada tiene_descuento que tome como parámetro una edad y devuelva True en caso de que la 
@@ -71,36 +78,39 @@ def tiene_descuento(edad):
 print(tiene_descuento(8))
 print(tiene_descuento(60))
 print(tiene_descuento(30))
+print()
 
 #Ejercicio 9
-    # Escribir una función xor que tome como parámetro dos booleanos y retorne el booleano correspondiente con la tabla.
+# Escribir una función xor que tome como parámetro dos booleanos y retorne el booleano correspondiente con la tabla.
 print ("Ejercicio 9")
 def disyuncion_exclusiva(a, b):
     if a == True and b == True: 
         resultado = False
-        print (resultado)
+        return resultado
     elif a == True and b == False: 
         resultado = True
-        print (resultado)
+        return resultado
     elif a == False and b == True: 
         resultado = True
-        print (resultado)
+        return resultado    
     elif a == False and b == False: 
         resultado = False
-        print (resultado)
-disyuncion_exclusiva(True, False)
-disyuncion_exclusiva(False, False)
+        return resultado
+print(disyuncion_exclusiva(True, False))
+print(disyuncion_exclusiva(False, False))
 def xor(a, b):
     return (a and not b) or (not a and b)
-xor(True, False)
-xor(True, True)
+print(xor(True, False))
+print(xor(True, True))
+print()
 
 #Ejercicio 10
 # Escribir una función que reciba un nombre y un apellido y devuelva un saludo de bienvenida para esa persona.
 def saludo():
     nombre = input("Dame tu nombre y apellido: ")
-    print (f"Hola {nombre}, bienvenido a python")
-saludo()
+    return "Hola " + nombre + " bienvenido a python"
+print(saludo())
+print()
 
 #Ejercicio 11
 # Escribir una función que tome como parámetro un string y que, si empieza con la letra "H", nos devuelva la 
@@ -113,27 +123,29 @@ def longitud_h(palabra):
         print("La palabra no comienza con h")
 longitud_h("Hongo")
 longitud_h("Tango")
+print()
 
 #Ejercicio 12
 # Escribir una función que reciba como parámetro un string y nos diga si el string empieza 
 # con "Buenos" o "Buenas".
 def buenas_o_buenos(frase):
     if frase.startswith("Buenos"):
-        print ("La frase comienza con Buenos")
+        return "La frase comienza con Buenos"
     elif frase.startswith('Buenas'):
-        print ("La frase comienza con Buenas")
+        return "La frase comienza con Buenas"
     else:
         return None
-buenas_o_buenos("Buenos dias")
-buenas_o_buenos("Buenas tardes")
+print(buenas_o_buenos("Buenos dias"))
+print(buenas_o_buenos("Buenas tardes"))
 
 def buena(frase):
     if frase[4] == "a":
-        print ("La frase comienza con Buenas")
+        return "La frase comienza con Buenas"
     elif frase[4] == "o":
-        print ("La frase comienza con Buenos")
-buena("Buenos dias")
-buena("Buenas tardes")
+        return "La frase comienza con Buenos"
+print (buena("Buenos dias"))
+print(buena("Buenas tardes"))
+print()
 
 #Ejercicio 13
 # Escribir una función llamada es_multiplo que reciba dos números y diga si el segundo
@@ -144,7 +156,7 @@ def es_multiplo(numero1, numero2):
     else:
         print(f"{numero2} NO es multiplo de {numero1}")
 es_multiplo(10, 2)
-
+print()
 #Ejercicio 14
 # Escribir una función que nos diga si un número es par, impar o cero.
 def par_impar(numero1):
@@ -157,7 +169,7 @@ def par_impar(numero1):
 par_impar(4)
 par_impar(5)
 par_impar(0)
-
+print()
 #Ejercicio 15
 # Escribir una función que tome como parámetro una frase y nos diga cuántas "a" (o "A") hay en la frase,
 # utilizando for.
@@ -168,7 +180,7 @@ def contador_de_a(frase):
             contador += 1
     print(f"La cantidad de letras a que hay en la frase son {contador}")
 contador_de_a("Habia una vez")
-
+print()
 #Ejercicio 16
 # Escribir una función que tome como valor una cantidad de dinero y nos diga por cuántos meses podemos 
 # subsistir con ese dinero, tomando en cuenta que se gastan 60000 pesos por mes.
