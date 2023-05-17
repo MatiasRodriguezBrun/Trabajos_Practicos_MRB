9059402d33df6498dad9109f4a2111dd8ad3c14e
MatiasRodriguezBrun <matias.rodriguez.brun@gmail.com>
2023-05-17 13:29:49 -0300

    actualizo

---
+++ Clases_2do_año/Clases/Tareas/clase_practica_manejo_de_errores.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Creá una función denominada eneavo que tenga como argumento un número e imprima el resultado de la división de 
+#1 por ese número
+
+def eneavo(numero):
+    try: 
+        print (1/numero)
+    except:
+        print ("No se puede dividir por ese numero")
+    
+eneavo(9)
+eneavo(0) 
+    
+
+
