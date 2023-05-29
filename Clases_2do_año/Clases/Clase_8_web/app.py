f478837ccaee46000d11948a66d60a0f8df626c6
MatiasRodriguezBrun <matias.rodriguez.brun@gmail.com>
2023-05-29 09:03:16 -0300

    actualizo

--- Clases_2do_año/Clases/Clase_8_web/app.py
+++ Clases_2do_año/Clases/Clase_8_web/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 prendas = [
     {"id": 1, "tipo": "pantalon", "talle": 38},
     {"id": 2, "tipo": "pantalon", "talle": 48}
     ]
+
 @app.get("/")
 def home():
     return "<p>Te damos la bienvenida a MacoWins</p>" #<p> etiqueta de parrafo
+
+@app.get("/prendas/")
+def get_all_prendas():
+    return f"<p>Mostrando todas las prendas</p>"
 
 #armar la ruta /prendas que muetsre todos los items de prendas
