a0e0b3e8d08df8fd41acbdc92c8edca2b7d12ce4
MatiasRodriguezBrun <matias.rodriguez.brun@gmail.com>
2023-06-16 16:18:05 -0300

    actualizo, no se xq no cargan las img

--- Practicas/Flask_exercise/app.py
+++ Practicas/Flask_exercise/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.get("/conclusiones/")
+def get_conclusiones():
+    return render_template("plan_2023.html")
+
 @app.get("/resultados/") 
 def get_resultados():
     return render_template("resultados.html")
-
-
-# plan_2023
-
